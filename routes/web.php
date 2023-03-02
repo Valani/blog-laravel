4d25c4cf17f25dfb879ca72948d71a93689add5f
Valani <cas23473@gmail.com>
2023-03-02 18:09:14 +0200

    refactor routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\Main\IndexController as AdminIndex;
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace'], function () {
-    Route::get('/', IndexController::class);
+Route::controller(IndexController::class)->group(function () {
+    Route::get('/', '__invoke');
 });
 
-Auth::routes();
+Route::prefix('admin')->group(function () {
+    Route::controller(AdminIndex::class)->group(function () {
+        Route::get('/', '__invoke');
+    });
+});
+
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
